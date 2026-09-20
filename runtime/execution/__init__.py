f5449a49be451da4b81ad722f02f674026bdcc92
cxjchelsea <147217475+cxjchelsea@users.noreply.github.com>
2026-09-20 13:57:57 +0800

    M5 readiness: export execution contracts and protocols

---
+++ runtime/execution/__init__.py
@@ -0,0 +1,69 @@
+"""M5 Execution Framework internal contracts and protocols."""
+
+from runtime.execution.control import (
+    ExecutionControlSignal,
+    ExecutionControlSignalSource,
+    ExecutionControlSignalType,
+)
+from runtime.execution.models import (
+    ActivityInstance,
+    ActivityStatus,
+    ExecutionErrorRecord,
+    ExecutionEventRecord,
+    ExecutionRecord,
+    M5SkillResult,
+    M5ToolResult,
+    M5WorkflowResult,
+    SkillExecutionRequest,
+    SkillExecutionStatus,
+    StepExecutionStatus,
+    ToolExecutionStatus,
+    ToolInvocationRequest,
+    WorkflowCheckpoint,
+    WorkflowExecutionRequest,
+    WorkflowExecutionStatus,
+)
+from runtime.execution.protocols import (
+    SkillImplementation,
+    ToolImplementation,
+    WorkflowImplementation,
+)
+from runtime.execution.stores import (
+    ExecutionStateStore,
+    IdempotencyRecord,
+    IdempotencyStatus,
+    IdempotencyStore,
+    ResourceLockProvider,
+    WorkflowCheckpointStore,
+)
+
+__all__ = [
+    "ActivityInstance",
+    "ActivityStatus",
+    "ExecutionControlSignal",
+    "ExecutionControlSignalSource",
+    "ExecutionControlSignalType",
+    "ExecutionErrorRecord",
+    "ExecutionEventRecord",
+    "ExecutionRecord",
+    "ExecutionStateStore",
+    "IdempotencyRecord",
+    "IdempotencyStatus",
+    "IdempotencyStore",
+    "M5SkillResult",
+    "M5ToolResult",
+    "M5WorkflowResult",
+    "ResourceLockProvider",
+    "SkillExecutionRequest",
+    "SkillExecutionStatus",
+    "SkillImplementation",
+    "StepExecutionStatus",
+    "ToolExecutionStatus",
+    "ToolImplementation",
+    "ToolInvocationRequest",
+    "WorkflowCheckpoint",
+    "WorkflowCheckpointStore",
+    "WorkflowExecutionRequest",
+    "WorkflowExecutionStatus",
+    "WorkflowImplementation",
+]

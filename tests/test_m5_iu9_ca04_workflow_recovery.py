7c435404c5e77a751553e2a907f855bcc54420b4
cxjchelsea <147217475+cxjchelsea@users.noreply.github.com>
2026-09-23 15:50:54 +0800

    test(m5-iu9-ca06): use exact owner supersession protocol

--- tests/test_m5_iu9_ca04_workflow_recovery.py
+++ tests/test_m5_iu9_ca04_workflow_recovery.py
@@ -214,7 +214,7 @@ class InflightStore:
             reason_codes=("NO_ACTIVE",),
         )
 
-    async def supersede_orphaned_owner_frames(self, **kwargs):
+    async def supersede_orphaned_owner_frame(self, **kwargs):
         return InFlightRecoveryTransitionDecision(
             status=InFlightRecoveryTransitionStatus.NO_ACTIVE,
             reason_codes=("NO_ORPHANED_OWNER_FRAME",),
